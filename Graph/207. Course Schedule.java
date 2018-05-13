34159b788eff5519b54009d04286fa5fe4b0605d
kukubao207 <502914814@qq.com>
2018-05-13 17:48:36 +0800

    周日-拓扑排序

--- Graph/207. Course Schedule.java
+++ Graph/207. Course Schedule.java
@@ -14,6 +14,7 @@ Explanation: There are a total of 2 courses to take.
 To take course 1 you should have finished course 0. So it is possible.
 Example 2:
 
+Input: 2, [[1,0],[0,1]]
 Output: false
 Explanation: There are a total of 2 courses to take.
 To take course 1 you should have finished course 0, and to take course 0 you should
