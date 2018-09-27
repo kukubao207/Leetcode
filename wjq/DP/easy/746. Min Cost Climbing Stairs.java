956b242ca03bbf90b5499772651a028736707a1b
王加琪 <502914814@qq.com>
2018-09-27 21:46:25 +0800

    update others code

--- wjq/DP/easy/746. Min Cost Climbing Stairs.java
+++ wjq/DP/easy/746. Min Cost Climbing Stairs.java
@@ -29,3 +29,11 @@ class Solution {
         return dp[cost.length];
     }
 }
+
+大佬的思路
+public int minCostClimbingStairs(int[] cost) {
+    for (int i = 2; i < cost.length; i++) {
+        cost[i] += Math.min(cost[i-1], cost[i-2]);
+    }
+    return Math.min(cost[cost.length-1], cost[cost.length-2]);
+}
