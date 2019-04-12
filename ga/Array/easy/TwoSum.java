0a9af9156446556b8820446489d0cc6324f627b0
kukubao207 <502914814@qq.com>
2019-04-12 16:01:10 +0800

    first

---
+++ ga/Array/easy/TwoSum.java
@@ -0,0 +1,13 @@
+package Array.easy;
+
+public class TwoSum {
+    public static void main(String[] args){
+        int nums[] = {2, 7, 11, 15};
+        int target = 9;
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        return res;
+    }
+}
