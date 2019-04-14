ac4696701e3ff2ba27b0dcdd9938f928cf1cec9e
kukubao207 <502914814@qq.com>
2019-04-14 18:49:09 +0800

    捏成你的形状

---
+++ ga/Array/easy/PascalTriangle_II.java
@@ -0,0 +1,17 @@
+package Array.easy;
+//119. Pascal's Triangle II
+//给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+//        在杨辉三角中，每个数是它左上方和右上方的数的和。
+//
+//        示例:
+//
+//        输入: 3
+//        输出: [1,3,3,1]
+//        进阶：
+//
+//        你可以优化你的算法到 O(k) 空间复杂度吗？
+public class PascalTriangle_II {
+    public static void main(String args[]){
+
+    }
+}
