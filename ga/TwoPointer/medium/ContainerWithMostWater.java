41573ccd1187d4a2cb5ed6532c6d0a62aa2cbe2b
kukubao207 <502914814@qq.com>
2019-04-24 09:51:47 +0800

    I see your face

---
+++ ga/TwoPointer/medium/ContainerWithMostWater.java
@@ -0,0 +1,8 @@
+package TwoPointer.medium;
+//11. Container With Most Water
+//        给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+//
+//        说明：你不能倾斜容器，且 n 的值至少为 2。
+public class ContainerWithMostWater {
+    //小爱写完之后证明该算法的正确性,写下你的证明过程
+}
--- test/niuke/Main.java
+++ test/niuke/Main.java
@@ -1,23 +1,68 @@
 package niuke;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class Main implements Runnable{
-    int x, y, z;
-    long a[] = new long[55];
-    long b[] = new long[55];
-    public enum EEnum{
-        PREFECT,
-        WORLD,
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer m = sc.nextInt();
+        Integer n = sc.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        solve(a, m, n);
     }
-    public void run(){
-        System.out.println("123");
-    }
-    public void run(Runnable r){
-        System.out.println("12345");
-    }
-    public static void main(String[] args){
-        new Thread(new Main()).start();
+
+    public static void solve(int[][] a, int m, int n) {
+        // 降序比较器
+        Comparator<Map.Entry<Integer, Integer>> valueComparator = new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1,
+                               Map.Entry<Integer, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        };
+
+        Map<Integer, Integer> count1 = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = i % 2; j < n; j += 2)
+                count1.put(a[i][j], count1.getOrDefault(a[i][j], 0) + 1);
+        }
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(count1.entrySet());
+        Collections.sort(list, valueComparator);
+
+
+        Map<Integer, Integer> count2 = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = (i + 1) % 2; j < n; j += 2)
+                count2.put(a[i][j], count2.getOrDefault(a[i][j], 0) + 1);
+        }
+        List<Map.Entry<Integer, Integer>> list2 = new ArrayList<Map.Entry<Integer, Integer>>(count2.entrySet());
+        Collections.sort(list2, valueComparator);
+
+
+        if (list.get(0).getKey() != list2.get(0).getKey()) {
+            System.out.println(m * n - list.get(0).getValue() - list2.get(0).getValue());
+            return;
+        } else {
+            if (list.size() == 1 && list2.size() == 1) {
+                System.out.println(m * n - Math.max(list.get(0).getValue(), list2.get(0).getValue()));
+                return;
+            } else if (list.size() != 1 && list2.size() != 1) {
+                System.out.println(m * n - Math.max(list.get(0).getValue() + list2.get(1).getValue(), list.get(1).getValue() + list2.get(0).getValue()));
+                return;
+            } else if (list.size() == 1) {
+                System.out.println(m * n - list.get(0).getValue() - list.get(1).getValue());
+                return;
+            } else if (list2.size() == 1) {
+                System.out.println(m * n - list.get(1).getValue() - list.get(0).getValue());
+                return;
+            }
+        }
     }
 
 }
