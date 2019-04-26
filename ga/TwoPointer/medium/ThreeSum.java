4742ea2a13d4b95e5c67319b63d3ee2b99bca971
biggrass <hhga2014@163.com>
2019-04-26 23:22:22 +0800

    ........

--- ga/TwoPointer/medium/ThreeSum.java
+++ ga/TwoPointer/medium/ThreeSum.java
@@ -1,4 +1,9 @@
 package TwoPointer.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //15. 3Sum
 //给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
 //
@@ -12,5 +17,59 @@ package TwoPointer.medium;
 //  [-1, -1, 2]
 //]
 public class ThreeSum {
+    public static void main(String args[]){
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
 
+    }
+
+    public static List<List<Integer>> twoSum(int nums[], int l ,int r, int target){
+        int i = l;
+        int j = r;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        while(l < r){
+            if(nums[l] + nums[r] < target)
+                l++;
+            else if(nums[l] + nums[r] > target)
+                r--;
+            else{
+                List<Integer> subRes = new ArrayList<Integer>();
+                subRes.add(-target);
+                subRes.add(nums[l]);
+                subRes.add(nums[r]);
+                res.add(subRes);
+                l++;
+                r--;
+            }
+        }
+        return res;
+    }
+
+    //三数之和问题可以转换为二数之和问题
+    //a+b+c = 0 --> a+b = -c
+    //时间复杂度O(n*n)
+    //空间复杂度O(n*n)
+    public static List<List<Integer>> threeSum(int nums[]){
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for(int i = 0; i < nums.length - 2; i++){
+            List subRes = twoSum(nums, i + 1, nums.length - 1, -nums[i]);
+            res.addAll(subRes);
+        }
+        List<List<Integer>> finalRes = new ArrayList<List<Integer>>();
+        if(res.size() > 0)
+            finalRes.add(res.get(0));
+        for(int i = 1; i < res.size(); i++){
+            int j = i - 1;
+            for(j = i - 1; j >= 0; j--){
+                if(res.get(i).equals(res.get(j))){
+                    break;
+                }
+            }
+            if(j == -1){
+                finalRes.add(res.get(i));
+            }
+        }
+        return finalRes;
+    }
 }
--- ga/TwoPointer/medium/ThreeSumCloest.java
+++ ga/TwoPointer/medium/ThreeSumCloest.java
@@ -1,4 +1,7 @@
 package TwoPointer.medium;
+
+import java.util.Arrays;
+
 //16. 3Sum Closest
 //给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
 //
@@ -6,5 +9,43 @@ package TwoPointer.medium;
 //
 //与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
 public class ThreeSumCloest {
+    public static void main(String args[]){
+        int[] nums = new int[]{-1,2,1,-4};
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
 
+    }
+    //先排序, 然后遍历, 将三数之和改变为二数之和，然后内部使用双指针
+    //时间复杂度O(n*n)，空间复杂度O(1)
+    public static int threeClose(int[] nums, int n, int target, int l ,int r) {
+        int min = Integer.MAX_VALUE;
+        int res = 0;
+        while(l < r){
+            if(Math.abs(nums[l] + nums[r] + n - target) < min){
+                min = Math.abs(nums[l] + nums[r] + n - target);
+                res = nums[l] + nums[r] + n;
+            }
+            if(nums[l] + nums[r] + n > target)
+                r--;
+            else if(nums[l] + nums[r] + n < target)
+                l++;
+            else
+                return target;
+        }
+        return res;
+    }
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        int res = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < nums.length - 2; i++){
+            int subRes = threeClose(nums, nums[i], target, i + 1, nums.length - 1);
+            int m = Math.abs(subRes - target);
+            if(m < min){
+                min = m;
+                res = subRes;
+            }
+        }
+        return res;
+    }
 }
