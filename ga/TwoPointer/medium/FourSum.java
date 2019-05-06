7df67f637cbfa9abde5bb2ec417cc6012ddd3340
biggrass <hhga2014@163.com>
2019-05-06 21:37:35 +0800

    .......

--- ga/TwoPointer/medium/FourSum.java
+++ ga/TwoPointer/medium/FourSum.java
@@ -1,5 +1,7 @@
 package TwoPointer.medium;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //18. 4Sum
@@ -29,10 +31,52 @@ public class FourSum {
 
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
-        return null;
+        Arrays.sort(nums);
+        return kSum(nums, target, 4, 0);
     }
 
     //注意去重
-
-
+    //kSum问题
+    //nums是排序过的数组
+    public static List<List<Integer>> kSum(int[] nums, int target, int k, int start) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        int len = nums.length;
+        if(start >= len) return res;
+        if(k == 2){
+            int l = start;
+            int r = len - 1;
+            while(l < r){
+                if(nums[l] + nums[r] < target){//去重
+                    while(l < r && nums[l + 1] == nums[l])
+                        l++;
+                    l++;
+                }else if(nums[l] + nums[r] > target){
+                    while(l < r && nums[r - 1] == nums[r])
+                        r--;
+                    r--;
+                }else{
+                    res.add(new ArrayList<>(Arrays.asList(nums[l],nums[r])));//转换为arraylist
+                    while(l < r && nums[l + 1] == nums[l])
+                        l++;
+                    while(l < r && nums[r - 1] == nums[r])
+                        r--;
+                    l++;
+                    r--;
+                }
+            }
+        }
+        if(k > 2){
+            for(int i = start; i < len - k + 1; i++){
+                if(i > start && nums[i] == nums[i - 1]) continue;//去重
+                List<List<Integer>> temp = kSum(nums, target - nums[i], k - 1, i + 1);
+                if(temp != null){
+                    for(List<Integer> l : temp){
+                        l.add(0, nums[i]);
+                    }
+                    res.addAll(temp);
+                }
+            }
+        }
+        return res;
+    }
 }
