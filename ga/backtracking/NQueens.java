a83c39000a728d0dc7a22e25ee2690a0d583d443
biggrass <hhga2014@163.com>
2019-05-13 22:11:37 +0800

    ...

--- ga/backtracking/NQueens.java
+++ ga/backtracking/NQueens.java
@@ -23,5 +23,57 @@ package backtracking;
 //]
 //解释: 4 皇后问题存在两个不同的解法。
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class NQueens {
+    public static void main(String args[]){
+       int n = 8;
+        System.out.println(solveNQueens(n));
+    }
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<int[]> res = new ArrayList<int[]>();
+        List<List<String>> sol = new ArrayList<List<String>>();
+        int[] position = new int[n + 1];
+        String str = "";
+        for(int i = 0; i < n; i++)
+            str = str + ".";
+        queen(res, position, 1, n);
+        for(int[] r: res){//一个r代表一个解
+            List<String> ss = new ArrayList<String>();
+            for(int p: r){//代表每个解的每一行
+                if (p != 0){
+                    StringBuilder strBuilder = new StringBuilder(str);
+                    strBuilder.setCharAt(p - 1, 'Q');
+                    ss.add(strBuilder.toString());
+                }
+            }
+            sol.add(ss);
+        }
+        return sol;
+    }
+    public static boolean is_ok(int[] position, int row){
+        for(int i = 1; i  < row; i++){
+            if(position[i] == position[row] || Math.abs(position[i] - position[row]) == row - i){//保证不在同一行，这种情况不用考虑
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void queen(List<int[]> res, int[] position, int row, int n) {
+        if(row == n + 1){//一种解法生成
+            res.add(position);//最后输出的res不知道是什么鬼 感觉又覆盖, position感觉可以正确记录位置
+            System.out.println(Arrays.toString(position));
+        }
+        else{
+            for(int col = 1; col <= n; col++){//循环列
+                position[row] = col;//row行col列放置皇后
+                if(is_ok(position, row)){
+                    queen(res, position, row + 1, n);
+                }
+            }
+        }
+    }
 }
