2d8c23ac072111dcd5cb86ed788a317fd58d76af
biggrass <hhga2014@163.com>
2019-05-19 16:46:20 +0800

    美丽承诺被时间证明

--- ga/Array/medium/SearchA2DMatrix.java
+++ ga/Array/medium/SearchA2DMatrix.java
@@ -25,4 +25,44 @@ package Array.medium;
 //target = 13
 //输出: false
 public class SearchA2DMatrix {
+    //类似二分法
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        return searchMatrix(matrix, target, 0, matrix.length * matrix[0].length - 1);
+    }
+
+    private boolean searchMatrix (int [][]matrix, int target, int startIndex, int endIndex) {
+        while(startIndex <= endIndex){
+            int mid = (startIndex + endIndex) / 2;
+            int row = mid / matrix[0].length;
+            int col = mid - row * matrix[0].length;
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (target > matrix[row][col]) {
+                return searchMatrix(matrix, target, mid + 1, endIndex);
+            } else {
+                return searchMatrix(matrix, target, startIndex, mid -1);
+            }
+        }
+        return false;
+    }
+//选择右上角或者左下角进行比较
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] < target)
+                row++;
+            else if(matrix[row][col] > target)
+                col--;
+            else
+                return true;
+        }
+        return false;
+    }
 }
--- ga/Array/medium/SetMatrixZeroes.java
+++ ga/Array/medium/SetMatrixZeroes.java
@@ -1,4 +1,8 @@
 package Array.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //73. 矩阵置零
 //给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
 //
@@ -36,4 +40,95 @@ package Array.medium;
 //一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
 //你能想出一个常数空间的解决方案吗？
 public class SetMatrixZeroes {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZeroes(matrix);
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                System.out.println(matrix[i][j]);
+            }
+        }
+    }
+    //空间复杂度O(n)
+    public static int[][] setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        List<Integer> col = new ArrayList<Integer>();
+        boolean judge = false;
+        for(int i = 0; i < m; i++){
+            judge = false;
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    judge = true;
+                    if(!col.contains(j))
+                        col.add(j);
+                }
+            }
+            if(judge){
+                for(int j = 0; j < n; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int c : col){
+            for(int i = 0; i < m; i++){
+                matrix[i][c] = 0;
+            }
+        }
+        return matrix;
+    }
+
+
+
+    //利用数组的首行和首列来记录 0 值。从数组下标的 A[1][1] 开始遍历，
+    // 两个布尔值记录首行首列是否需要置0
+    //空复杂度O(2)
+    public void setZeroes1(int[][] matrix) {
+        boolean rowFlag = false;
+        for(int i = 0; i < matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                rowFlag = true;
+                break;
+            }
+        }
+        boolean colFlag = false;
+        for(int i = 0; i < matrix.length; i++){
+            if(matrix[i][0] == 0){
+                colFlag = true;
+                break;
+            }
+        }
+        for(int i = 1; i < matrix.length; i++){
+            for(int j = 1; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < matrix.length; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 0; j < matrix[0].length; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                for(int j = 0; j < matrix.length; j++){
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        if(rowFlag){
+            for(int i = 0; i < matrix[0].length; i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(colFlag){
+            for(int i = 0; i < matrix.length; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
