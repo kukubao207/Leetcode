c4dbe7da45fad6e2be4e30bf0e34e5dfa8d19c08
biggrass <hhga2014@163.com>
2019-05-28 15:22:34 +0800

    You are my today and all of my tomorrows.

--- ga/LinkedList/easy/MergeTwoSortedLists.java
+++ ga/LinkedList/easy/MergeTwoSortedLists.java
@@ -8,5 +8,47 @@ package LinkedList.easy;
 //输出：1->1->2->3->4->4
 //我爱你，你是我的朱丽叶~
 public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            }else{
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        if(l1 != null)
+            cur.next = l1;
+        else if(l2 != null)
+            cur.next = l2;
+        return dummyHead.next;
+    }
+
+    //递归
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        else if(l2 == null)
+            return l1;
+        else{
+            if(l1.val < l2.val){
+                l1.next = mergeTwoLists1(l1.next, l2);
+                return l1;
+            }else{
+                l2.next = mergeTwoLists1(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 
 }
--- ga/LinkedList/hard/MergeKSortedLists.java
+++ ga/LinkedList/hard/MergeKSortedLists.java
@@ -1,4 +1,7 @@
 package LinkedList.hard;
+
+import java.util.Arrays;
+
 //23. 合并K个排序链表
 //合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
 //
@@ -13,5 +16,52 @@ package LinkedList.hard;
 //输出: 1->1->2->3->4->4->5->6
 //我愿意，变成你的梁山伯~
 public class MergeKSortedLists {
+    //递归
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        if (lists.length == 1) return lists[0];
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        for(int i = 0; i < lists.length; i++){
+            cur.next = mergeTwoLists(cur.next, lists[i]);
+        }
+        return dummyHead.next;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        else if(l2 == null)
+            return l1;
+        else{
+            if(l1.val < l2.val){
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            }else{
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+
+    //分治
+    public ListNode mergeKLists1(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        if (lists.length == 1) return lists[0];
+        if (lists.length == 2) return mergeTwoLists(lists[0], lists[1]);
+        int mid = lists.length / 2;
+        ListNode[] l1 = new ListNode[mid];
+        for(int i = 0; i < mid; i++)
+            l1[i] = lists[i];
+        ListNode[] l2 = new ListNode[lists.length - mid];
+        for(int i = mid, j = 0; i < lists.length; i++, j++)
+            l2[j] = lists[i];
+        return mergeTwoLists(mergeKLists1(l1), mergeKLists1(l2));
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 
 }
