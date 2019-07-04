00fc49945a2fbf23ab8d5e516b92a2e372b15b43
wangjiaqi.19960310 <wangjiaqi.19960310@bytedance.com>
2019-07-04 19:49:15 +0800

    mua

---
+++ ga/Tree/easy/MaximumDepthOfNaryTree.java
@@ -0,0 +1,23 @@
+package Tree.easy;
+//559. Maximum Depth of N-ary Tree
+//Given a n-ary tree, find its maximum depth.
+//
+//The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+//
+//For example, given a 3-ary tree:
+//
+//
+//
+//
+//
+//
+//We should return its max depth, which is 3.
+//
+//
+//
+//Note:
+//
+//The depth of the tree is at most 1000.
+//The total number of nodes is at most 5000.
+public class MaximumDepthOfNaryTree {
+}
