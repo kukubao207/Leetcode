6d53d8a741fd4003e8ea99e750b1a6c67c23572a
wangjiaqi.19960310 <wangjiaqi.19960310@bytedance.com>
2019-07-05 20:12:45 +0800

    light my life

---
+++ ga/Tree/easy/NaryTreePostorderTraversal.java
@@ -0,0 +1,20 @@
+package Tree.easy;
+//590. N-ary Tree Postorder Traversal
+//Given an n-ary tree, return the postorder traversal of its nodes' values.
+//
+//For example, given a 3-ary tree:
+//
+//
+//
+//
+//
+//
+//
+//Return its postorder traversal as: [5,6,3,2,4,1].
+//
+//
+//Note:
+//
+//Recursive solution is trivial, could you do it iteratively?
+public class NaryTreePostorderTraversal {
+}
---
+++ ga/Tree/easy/NaryTreePreorderTraversal.java
@@ -0,0 +1,21 @@
+package Tree.easy;
+//589. N-ary Tree Preorder Traversal
+//Given an n-ary tree, return the preorder traversal of its nodes' values.
+//
+//For example, given a 3-ary tree:
+//
+//
+//
+//
+//
+//
+//
+//Return its preorder traversal as: [1,3,5,6,2,4].
+//
+//
+//
+//Note:
+//
+//Recursive solution is trivial, could you do it iteratively?
+public class NaryTreePreorderTraversal {
+}
