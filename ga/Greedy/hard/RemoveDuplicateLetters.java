dd15ac92f226ba487a8769dd96363db3baec61f8
wangjiaqi.19960310 <wangjiaqi.19960310@bytedance.com>
2019-07-26 10:33:31 +0800

    牙疼

--- ga/Greedy/hard/RemoveDuplicateLetters.java
+++ ga/Greedy/hard/RemoveDuplicateLetters.java
@@ -35,3 +35,17 @@ public class RemoveDuplicateLetters {
 }
 
 //这个题是春招时候拼多多的第二个题。
+// 删除字符串，
+//    public String removeDuplicateLetters(String s) {
+//        int[] cnt = new int[26];
+//        for (int i = 0; i < s.length(); i++)
+//            cnt[s.charAt(i) - 'a']++;
+//        int pos = 0; // the position for the smallest s[i]
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) < s.charAt(pos))
+//                pos = i;
+//            if (--cnt[s.charAt(i) - 'a'] == 0)
+//                break;
+//        }
+//        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
+//    }
