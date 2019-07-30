bb4356d01cb198d31b45306ae58f02e31e0253fd
biggrass <hhga2014@163.com>
2019-07-30 21:06:31 +0800

    pdd

--- ga/pdd/exam3.java
+++ ga/pdd/exam3.java
@@ -1,40 +1,72 @@
 package pdd;
+import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
-
+ 一共有N个待执行的任务，每个任务需要Pi的时间完成。同时每个任务之间存在一定的依赖关系。
+ 比如任务1可能依赖任务2和任务3，那么任务1必须在任务2和任务3完成之后执行
+ 多多鸡只有一个人，所以同时只能执行一个任务，同时在任务完成之前不能暂停切换去执行其他任务
+ 多多鸡希望最小化任务的平均返回时长。一个任务的返回时长的定义是任务执行完成时刻减去平台接收到该任务的时刻。
+ 在零时刻，所有N个任务都已经被平台接收
+ 请安排任务执行顺序，使得平均返回时长最小
  */
+//输入
+//  5 6（N：任务数量 ； M：M个任务以来关系）
+//  1 2 1 1 1（第i个数表示第i个任务的处理时间Pi）
+//  (M个依赖关系)
+//  1 2
+//  1 3
+//  1 4
+//  2 5
+//  3 5
+//  4 5
+//输出
+//  1 3 4 2 5
 public class exam3 {
+    //拓扑排序+优先队列
     public static void main(String[] args) {
-        //按照长度逆序排序 https://www.cnblogs.com/javahyj/p/5305331.html学习一下Java怎么逆序排序
-        //然后输入到函数
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] p = new int[n + 1];
+        int[] inDegree = new int[n + 1];//入度//拓扑排序
+        List<Integer>[] dependence = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            p[i] = sc.nextInt();
+            dependence[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            dependence[a].add(b);//依赖关系  可能一对多
+            inDegree[b]++;
+        }
+        Queue<Pair> queue = new PriorityQueue<Pair>((o1,o2) -> {
+            if(o1.getKey() == o2.getKey())//时间一样长的按原始的默认顺序
+                return (int)o1.getValue() - (int)o2.getValue();
+            else return (int)o1.getKey() - (int)o2.getKey();//任务时间短的优先
+        });
+        for (int i = 1; i <= n; i++) {//先存入入度为0的点
+            if(inDegree[i] == 0)//入度为0的才存到队列中
+                queue.add(new Pair(p[i], i));
+        }
+        int[] res = new int[n];
+        int index = 0;
+        while(!queue.isEmpty()){
+            Pair pair = queue.poll();
+            res[index++] = (int)pair.getValue();
+            for(int d: dependence[(int)pair.getValue()]){
+                if(--inDegree[d] == 0)//入度为0的加进优先队列
+                    queue.add(new Pair(p[d], d));
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(i == n - 1)
+                System.out.print(res[i]);
+            else
+                System.out.print(res[i] + " ");
+        }
     }
 
-    //先按照length排序（从大到小）
-    public int maxHeight(int[] length, int[] weight) {
-        List<Integer> leftWeight = new ArrayList<Integer>();
-        int n = length.length;
-        int max = 0;
-        int res = 0;
-        for(int i = 0; i < n; i++){
-            leftWeight.add(7 * weight[i]);
-            max = 1;
-            for(int j = i + 1; j < n; j++){
-                for(Integer left: leftWeight){
-                    if(weight[j] > left)
-                        break;
-                    else{
-                        left = left - weight[j];
-                    }
-                }
-                max++;
-            }
-            res = Math.max(max, res);
-        }
-        return res;
-    }
 }
--- ga/pdd/exam4.java
+++ ga/pdd/exam4.java
@@ -1,11 +1,109 @@
 package pdd;
 
-/**
+import javafx.util.Pair;
 
+import java.util.*;
+
+/**
+ 搭积木游戏。有N个长方体积木，每个积木的高都是1，长宽都是L，重量为Wi
+ 用这些积木搭金字塔，金字塔的每一层是由且仅由一块积木组成，同时每一层的积木边长都严格比其下方的积木小
+ 每块积木只能承受自身重量的7倍，若超过7倍自重，搭建的金字塔会因此变得不稳定
+ 请计算，最高可以搭建一个多高的金字塔？
  */
 public class exam4 {
     public static void main(String[] args) {
+        //按照长度逆序排序 https://www.cnblogs.com/javahyj/p/5305331.html学习一下Java怎么逆序排序
+        //nextInt自动去掉enter键
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] length = new int[n];
+        int[] weight = new int[n];
+        for (int i = 0; i < n; i++)
+            length[i] = sc.nextInt();
+        for (int i = 0; i < n; i++)
+            weight[i] = sc.nextInt();
 
+        //Pair<length,weight> 按照length从大到小排列，length相等的时候按weight从大到小
+//        Queue<Pair> queue = new PriorityQueue<Pair>(new Comparator<Pair>() {
+//            @Override
+//            public int compare(Pair o1, Pair o2) {
+//                if(o1.getKey() == o2.getKey()){
+//                    return (int)o2.getValue() - (int)o1.getValue();
+//                }
+//                else{
+//                   return (int)o2.getKey() - (int)o1.getKey();
+//                }
+//            }
+//        });
+        Queue<Pair> queue = new PriorityQueue<Pair>((o1, o2) -> {return (int)o2.getKey() - (int)o1.getKey();});
+        for(int i = 0 ; i < n; i++){
+            queue.add(new Pair(length[i], weight[i]));
+        }
+        int i = 0;
+        while(queue.size() > 0){
+            Pair pair = queue.poll();
+            length[i] = (int)pair.getKey();
+            weight[i] = (int)pair.getValue();
+            i++;
+        }
+//        System.out.print(Arrays.toString(length));
+//        System.out.print(Arrays.toString(weight));
+        //然后输入到函数
+
+        System.out.print(maxHeight(length, weight, new ArrayList<>(), 0));
+    }
+    public static int maxHeight(int[] length, int[] weight, List<Integer> leftWeight, int start) {
+        int n = length.length;
+        if (start == n)
+            return 0;
+        for (int i = start; i < n; i++) {
+            List tmp = leftWeight;
+            for (int k = 0; k < leftWeight.size(); k++) {
+                if (weight[i] > leftWeight.get(k) || (i - 1 >= 0 && length[i] >= length[i - 1])) {
+                    leftWeight = tmp;
+                    return maxHeight(length, weight, leftWeight, start + 1);
+                } else {
+                    leftWeight.set(k, leftWeight.get(k) - weight[i]);
+                }
+            }
+            leftWeight.add(7 * weight[i]);
+//            System.out.print(leftWeight);
+            return Math.max(1 + maxHeight(length, weight, leftWeight, start + 1), maxHeight(length, weight, tmp, start + 1));
+        }
+        return -1;
     }
 
+    //先按照length排序（从大到小）
+    //这里有问题 可能满足条件的情况下，我依旧不选择这块积木
+//    public static int maxHeight(int[] length, int[] weight) {
+//        List<Integer> leftWeight = new ArrayList<Integer>();
+//        int n = length.length;
+//        int max = 0;
+//        int res = 0;
+//        boolean flag = true;
+//        for(int i = 0; i < n; i++){
+//            leftWeight.add(7 * weight[i]);
+//            max = 1;
+//            for(int j = i + 1; j < n; j++){
+//                for(int k = 0; k < leftWeight.size(); k++){
+//                    if(weight[j] > leftWeight.get(k)){
+//                        flag = false;
+//                        break;
+//                    }
+//                    else if(length[j] >= length[j - 1]){//排序以后有可能出现等于的情况
+//                        flag = false;
+//                        break;
+//                    }
+//                    else
+//                        leftWeight.set(k, leftWeight.get(k) - weight[j]);
+//                }
+//                leftWeight.add(7 * weight[j]);
+//                if(flag)
+//                    max++;
+//                flag = true;
+//            }
+//            res = Math.max(max, res);
+//        }
+//        return res;
+//    }
 }
