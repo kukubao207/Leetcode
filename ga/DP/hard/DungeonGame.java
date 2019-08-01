baac98093e7a534bda92056382bfac09cbb2c875
biggrass <hhga2014@163.com>
2019-08-01 20:46:23 +0800

    宝宝明天见

--- ga/DP/hard/DungeonGame.java
+++ ga/DP/hard/DungeonGame.java
@@ -24,4 +24,27 @@ package DP.hard;
 //The knight's health has no upper bound.
 //Any room can contain threats or power-ups, even the first room the knight enters and the bottom-right room where the princess is imprisoned.
 public class DungeonGame {
+    //dp[i][j]表示勇士处于第i行j列的时候成功救出公主需要的健康点数
+    //if(dungeon[i][j] <= 0)
+        //dp[i][j]= Math.min(dp[i][j+1],dp[i+1][j]) - dungeon[i][j]
+    //if(dungeon[i][j] > 0)
+        //dp[i][j]= Math.max(1,Math.min(dp[i][j+1],dp[i+1][j]) - dungeon[i][j])
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;//行数
+        int n = dungeon[0].length;//列数
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i + 1 == m && j + 1 == n)
+                    dp[i][j] = Math.max(1, 1 - dungeon[m - 1][n - 1]);
+                else if(i + 1 == m)
+                    dp[i][j]= Math.max(1, dp[i][j + 1] - dungeon[i][j]);
+                else if(j + 1 == n)
+                    dp[i][j]= Math.max(1, dp[i + 1][j] - dungeon[i][j]);
+                else
+                    dp[i][j]= Math.max(1, Math.min(dp[i][j + 1],dp[i + 1][j]) - dungeon[i][j]);
+            }
+        }
+        return dp[0][0];
+    }
 }
