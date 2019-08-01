52f1c0308db2f6c58a2fb1236df55ea73ddeca8d
wangjiaqi.19960310 <wangjiaqi.19960310@bytedance.com>
2019-08-01 10:41:25 +0800

    ai ni.

--- ga/DP/hard/DistinctSubsequences.java
+++ ga/DP/hard/DistinctSubsequences.java
@@ -40,37 +40,37 @@ package DP.hard;
 //babgbag
 //    ^^^
 public class DistinctSubsequences {
-}
-
-
-// 我的思路
-// dp做多了发现，大概都是用一个dp二维数组，然后自己先把正确答案填到设计的表格里，再去找规律。
-// 我这种解题方式是"猜"，也可以是说找规律，没有什么思维难度，主要就是找规律。
-class Solution {
-    public int numDistinct(String s, String t) {
-        if (s == null || t == null || s.equals("") || t.equals(""))
-            return 0;
-        int[][] dp = new int[t.length()][s.length()];
-        if (s.charAt(0) == t.charAt(0))
-            dp[0][0] = 1;
-        for (int i = 1; i < t.length(); i++) {
-            dp[i][0] = 0;
-        }
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == t.charAt(0))
-                dp[0][i] = dp[0][i - 1] + 1;
-            else
-                dp[0][i] = dp[0][i - 1];
-        }
-        for (int i = 1; i < t.length(); i++) {
-            for (int j = 1; j < s.length(); j++) {
-                if (s.charAt(j) == t.charAt(i)) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
-                } else {
-                    dp[i][j] = dp[i][j - 1];
+    // 我的思路
+    // dp做多了发现，大概都是用一个dp二维数组，然后自己先把正确答案填到设计的表格里，再去找规律。
+    // 我这种解题方式是"猜"，也可以是说找规律，没有什么思维难度，主要就是找规律。
+    class Solution {
+        public int numDistinct(String s, String t) {
+            if (s == null || t == null || s.equals("") || t.equals(""))
+                return 0;
+            int[][] dp = new int[t.length()][s.length()];
+            if (s.charAt(0) == t.charAt(0))
+                dp[0][0] = 1;
+            for (int i = 1; i < t.length(); i++) {
+                dp[i][0] = 0;
+            }
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == t.charAt(0))
+                    dp[0][i] = dp[0][i - 1] + 1;
+                else
+                    dp[0][i] = dp[0][i - 1];
+            }
+            for (int i = 1; i < t.length(); i++) {
+                for (int j = 1; j < s.length(); j++) {
+                    if (s.charAt(j) == t.charAt(i)) {
+                        dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                    } else {
+                        dp[i][j] = dp[i][j - 1];
+                    }
                 }
             }
+            return dp[t.length() - 1][s.length() - 1];
         }
-        return dp[t.length() - 1][s.length() - 1];
     }
 }
+
+
