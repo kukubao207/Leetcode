cda81b20b5758a1ba01cb7e12e1422695846e189
biggrass <hhga2014@163.com>
2019-08-06 21:44:11 +0800

    你要哄我哦

--- ga/String/ValidNumber.java
+++ ga/String/ValidNumber.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.ArrayList;
+
 //65. Valid Number
 //Validate if a given string can be interpreted as a decimal number.
 //
@@ -54,8 +56,331 @@ public class ValidNumber {
         }
     }
 
+
+    //1.把当前的输入分成几类，再用几个标志位来判断当前是否合法
+    //判断前置(只判断前面的是否正确 不然前后判断容易混乱)
     public static boolean isNumber(String s) {
-        return false;
+        s = s.trim();
+        boolean pointSeen = false;
+        boolean eSeen = false;
+        boolean numberSeen = false;
+        boolean numberAfterE = true;
+        for(int i = 0; i < s.length(); i++){
+            if('0' <= s.charAt(i) && s.charAt(i) <= '9'){
+                numberSeen = true;
+                numberAfterE = true;
+            }else if(s.charAt(i) == '.') {
+                if(eSeen || pointSeen)
+                    return false;
+                pointSeen = true;
+            }else if(s.charAt(i) == 'e'){
+                if(eSeen || !numberSeen)
+                    return false;
+                numberAfterE = false;
+                eSeen = true;
+            }else if(s.charAt(i) == '-' || s.charAt(i) == '+'){
+                if(i != 0 && s.charAt(i-1) != 'e') {
+                    return false;
+                }
+            }else
+                return false;
+        }
+        return numberSeen && numberAfterE;
+    }
+
+    //2.自动机
+    //状态机
+    public static boolean isNumber1(String s) {
+        int state = 0;
+        s = s.trim();//去除头尾的空格
+        //遍历所有字符，当做输入
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                //输入正负号
+                case '+':
+                case '-':
+                    if (state == 0) {
+                        state = 1;
+                    } else if (state == 4) {
+                        state = 6;
+                    } else {
+                        return false;
+                    }
+                    break;
+                //输入数字
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    //根据当前状态去跳转
+                    switch (state) {
+                        case 0:
+                        case 1:
+                        case 2:
+                            state = 2;
+                            break;
+                        case 3:
+                            state = 3;
+                            break;
+                        case 4:
+                        case 5:
+                        case 6:
+                            state = 5;
+                            break;
+                        case 7:
+                            state = 8;
+                            break;
+                        case 8:
+                            state = 8;
+                            break;
+                        default:
+                            return false;
+                    }
+                    break;
+                //小数点
+                case '.':
+                    switch (state) {
+                        case 0:
+                        case 1:
+                            state = 7;
+                            break;
+                        case 2:
+                            state = 3;
+                            break;
+                        default:
+                            return false;
+                    }
+                    break;
+                //e
+                case 'e':
+                    switch (state) {
+                        case 2:
+                        case 3:
+                        case 8:
+                            state = 4;
+                            break;
+                        default:
+                            return false;
+                    }
+                    break;
+                default:
+                    return false;
+            }
+        }
+        //橙色部分的状态代表合法数字
+        return state == 2 || state == 3 || state == 5 || state == 8;
+    }
+    //3.责任链
+    //每个类只判断一种类型。
+    // 比如判断是否是正数的类，判断是否是小数的类，判断是否是科学计数法的类，这样每个类只关心自己的部分，出了问题很好排查，而且互不影响。
+
+    //每个类都实现这个接口
+    interface NumberValidate{
+        boolean validate(String s);
+    }
+    //定义一个抽象类，用来检查一些基础的操作，是否为空，去掉首尾空格，去掉 +/-
+    //doValidate 交给子类自己去实现
+    abstract class  NumberValidateTemplate implements NumberValidate{
+        public boolean validate(String s)
+        {
+            if (checkStringEmpty(s))
+            {
+                return false;
+            }
+            s = checkAndProcessHeader(s);
+            if(s.length() == 0)
+                return false;
+            return doValidate(s);
+        }
+        private boolean checkStringEmpty(String s)
+        {
+            if (s.equals(""))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private String checkAndProcessHeader(String value)
+        {
+            value = value.trim();
+
+            if (value.startsWith("+") || value.startsWith("-"))
+            {
+                value = value.substring(1);
+            }
+            return value;
+        }
+        protected abstract boolean doValidate(String s);
+    }
+    //实现 doValidate 判断是否是整数
+    class IntegerValidate extends NumberValidateTemplate{
+        protected boolean doValidate(String integer){
+            {
+                for (int i = 0; i < integer.length(); i++)
+                {
+                    if(Character.isDigit(integer.charAt(i)) == false)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+    //实现 doValidate 判断是否是科学计数法
+    class SienceFormatValidate extends NumberValidateTemplate{
+        protected boolean doValidate(String s){
+            s = s.toLowerCase();
+            int pos = s.indexOf("e");
+            if (pos == -1)
+            {
+                return false;
+            }
+
+            if (s.length() == 1)
+            {
+                return false;
+            }
+            String first = s.substring(0, pos);
+            String second = s.substring(pos + 1, s.length());
+            if (validatePartBeforeE(first) == false || validatePartAfterE(second) == false){
+                return false;
+            }
+            return true;
+        }
+        private boolean validatePartBeforeE(String first){
+            if (first.equals("") == true)
+            {
+                return false;
+            }
+            if (checkHeadAndEndForSpace(first) == false)
+            {
+                return false;
+            }
+            NumberValidate integerValidate = new IntegerValidate();
+            NumberValidate floatValidate = new FloatValidate();
+            if (integerValidate.validate(first) == false && floatValidate.validate(first) == false)
+            {
+                return false;
+            }
+            return true;
+        }
+        private boolean checkHeadAndEndForSpace(String part)
+        {
+
+            if (part.startsWith(" ") ||
+                    part.endsWith(" "))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private boolean validatePartAfterE(String second){
+            if (second.equals("") == true)
+            {
+                return false;
+            }
+
+            if (checkHeadAndEndForSpace(second) == false)
+            {
+                return false;
+            }
+            NumberValidate integerValidate = new IntegerValidate();
+            if (integerValidate.validate(second) == false)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    //实现 doValidate 判断是否是小数
+    class FloatValidate extends NumberValidateTemplate {
+        protected boolean doValidate(String floatVal){
+            int pos = floatVal.indexOf(".");
+            if (pos == -1)
+            {
+                return false;
+            }
+
+            if (floatVal.length() == 1)
+            {
+                return false;
+            }
+            String first = floatVal.substring(0, pos);
+            String second = floatVal.substring(pos + 1, floatVal.length());
+            if (checkFirstPart(first) == true && checkFirstPart(second) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+        private boolean checkFirstPart(String first)
+        {
+            if (first.equals("") == false && checkPart(first) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private boolean checkPart(String part){
+            //. 1
+            if (Character.isDigit(part.charAt(0)) == false ||
+                    Character.isDigit(part.charAt(part.length() - 1)) == false)
+            {
+                return false;
+            }
+            NumberValidate nv = new IntegerValidate();
+            if (nv.validate(part) == false)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    //定义一个执行者，我们把之前实现的各个类加到一个数组里，然后依次调用
+    class NumberValidator implements NumberValidate {
+        private ArrayList<NumberValidate> validators = new ArrayList<NumberValidate>();
+        public NumberValidator()
+        {
+            addValidators();
+        }
+        private  void addValidators(){
+            NumberValidate nv = new IntegerValidate();
+            validators.add(nv);
+            nv = new FloatValidate();
+            validators.add(nv);
+            nv = new SienceFormatValidate();
+            validators.add(nv);
+        }
+        @Override
+        public boolean validate(String s)
+        {
+            for (NumberValidate nv : validators)
+            {
+                if (nv.validate(s) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+    public boolean isNumber2(String s) {
+        NumberValidate nv = new NumberValidator();
+        return nv.validate(s);
     }
 }
 
