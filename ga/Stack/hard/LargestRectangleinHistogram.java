c941b0f0e0ffa589b8bb1b2db2b554aa97d950a5
biggrass <hhga2014@163.com>
2019-08-10 16:25:00 +0800

    下周末见哦

--- ga/Stack/hard/LargestRectangleinHistogram.java
+++ ga/Stack/hard/LargestRectangleinHistogram.java
@@ -1,5 +1,7 @@
 package Stack.hard;
 
+import java.util.Stack;
+
 //84. Largest Rectangle in Histogram
 //Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
 //
@@ -25,29 +27,82 @@ public class LargestRectangleinHistogram {
     }
 
     public static void test() {
-        int[] a = {2, 1, 5, 6, 2, 3};
-        System.out.println(largestRectangleArea(a));
+        int[] a1 = {2, 1, 5, 6, 2, 3};
+        int[] a2 = {2, 1, 2};
+        int[] a3 = {1, 1};
+        System.out.println(largestRectangleArea1(a1));
+        System.out.println(largestRectangleArea1(a2));
+        System.out.println(largestRectangleArea1(a3));
     }
 
+//    public static int largestRectangleArea(int[] heights) {
+//        int[] left = new int[heights.length];
+//        int[] right = new int[heights.length];
+//        for (int i = 0; i < heights.length; i++) {
+//            int p = i - 1;
+//            while (p >= 0 && heights[p] >= heights[i])
+//                p = left[p];
+//            left[i] = p;
+//        }
+//        for (int i = heights.length - 1; i >= 0; i--) {
+//            int p = i + 1;
+//            while (p < heights.length && heights[p] >= heights[i])
+//                p = right[p];
+//            right[i] = p;
+//        }
+//        int maxArea = 0;
+//        for (int i = 0; i < heights.length; i++) {
+//            maxArea = Math.max(maxArea, heights[i] * (right[i] - left[i] - 1));
+//        }
+//        return maxArea;
+//    }
+
+    //1,暴力超时
     public static int largestRectangleArea(int[] heights) {
-        int[] left = new int[heights.length];
-        int[] right = new int[heights.length];
+        int max = 0;
+        for(int i = 0; i < heights.length; i++){
+            int width = 0;
+            int preMin = Integer.MAX_VALUE;
+            for(int j = i; j < heights.length; j++){
+                width++;
+                if(heights[j] < preMin || preMin == Integer.MAX_VALUE){
+                    max = Math.max(max, heights[j] * width);
+
+                }
+                else if(heights[j] >= preMin){
+                    max = Math.max(max, preMin * width);
+                }
+                preMin = Math.min(preMin, heights[j]);
+            }
+        }
+        return max;
+    }
+    /**https://blog.csdn.net/Zolewit/article/details/88863970
+     * 利用单调栈求解，总体思路是 以柱子i高度为矩形高度时所能形成最大面积
+     * （利用性质找出第i个柱子向左边和右边遍历时第一个比它低的柱子）
+     * 单调栈定义：只存高度递增的柱子
+     * 性质
+     * 出栈时：
+     * 那么如果单调栈为空了：说明没有比这个柱子更低的了（矩形宽度为这根柱子的序号：左边沿为0）
+     * 如果单调栈不为空：说明栈里面的柱子高度都小，那么左边沿为栈顶柱子的序号
+     *
+     * 矩形右边沿为i 因为你出栈 就说明你比别人低了，这已经是你能达到的面积极限了.出栈记录面积
+     * **/
+    public static int largestRectangleArea1(int[] heights) {
+        int[] heightn = new int[heights.length + 1];
         for (int i = 0; i < heights.length; i++) {
-            int p = i - 1;
-            while (p >= 0 && heights[p] >= heights[i])
-                p = left[p];
-            left[i] = p;
+            heightn[i] = heights[i];
         }
-        for (int i = heights.length - 1; i >= 0; i--) {
-            int p = i + 1;
-            while (p < heights.length && heights[p] >= heights[i])
-                p = right[p];
-            right[i] = p;
+        heightn[heights.length] = 0;   //最后增加个高度为0的柱子，以便把单调栈里面的都弹出去。
+        Stack<Integer> stack = new Stack<>(); //存储序号
+        int maxS = 0;
+        for (int i = 0; i < heightn.length; i++) {
+            while (!stack.isEmpty() && heightn[i] < heightn[stack.peek()]){  //一直出栈 直到遇见小的
+                int temp = stack.pop();
+                maxS = Math.max(maxS, ((stack.isEmpty() ? i : (i - stack.peek() - 1) ) * heights[temp]));//求包含heights[tmo]的面积
+            }
+            stack.push(i);//递增push
         }
-        int maxArea = 0;
-        for (int i = 0; i < heights.length; i++) {
-            maxArea = Math.max(maxArea, heights[i] * (right[i] - left[i] - 1));
-        }
-        return maxArea;
+        return maxS;
     }
 }
--- ga/Stack/hard/MaximalRectangle.java
+++ ga/Stack/hard/MaximalRectangle.java
@@ -1,4 +1,6 @@
 package Stack.hard;
+import java.util.Stack;
+
 import static Stack.hard.LargestRectangleinHistogram.largestRectangleArea;
 
 //85. Maximal Rectangle
@@ -28,22 +30,55 @@ public class MaximalRectangle {
     }
 
     public static int maximalRectangle(char[][] matrix) {
-        int maxRec = 0;
-        if (matrix.length == 0 || matrix == null)
-            return maxRec;
-        int dp[][] = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == '1') {
-                    if (i == 0 || dp[i - 1][j] == 0)
-                        dp[i][j] = 1;
-                    else
-                        dp[i][j] = dp[i - 1][j] + 1;
-                } else
-                    dp[i][j] = 0;
+        //1.利用84题， 对每一行都求出每个元素对应的高度，这个高度就是对应的连续1的长度
+        if(matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+        int m = matrix.length,n = matrix[0].length;
+        int[] heights = new int[n];
+        int res = 0;
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                heights[j] = matrix[i][j]== '0' ? 0 : 1 + heights[j];
             }
-            maxRec = Math.max(maxRec, largestRectangleArea(dp[i]));
+            res = Math.max(res, largestRectangleArea(heights));
         }
-        return maxRec;
+        return res;
+
+//        int maxRec = 0;
+//        if (matrix.length == 0 || matrix == null)
+//            return maxRec;
+//        int dp[][] = new int[matrix.length][matrix[0].length];
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                if (matrix[i][j] == '1') {
+//                    if (i == 0 || dp[i - 1][j] == 0)
+//                        dp[i][j] = 1;
+//                    else
+//                        dp[i][j] = dp[i - 1][j] + 1;
+//                } else
+//                    dp[i][j] = 0;
+//            }
+//            maxRec = Math.max(maxRec, largestRectangleArea(dp[i]));
+//        }
+//        return maxRec;
     }
+
+    public static int largestRectangleArea(int[] heights) {
+        int[] heightn = new int[heights.length + 1];
+        for (int i = 0; i < heights.length; i++) {
+            heightn[i] = heights[i];
+        }
+        heightn[heights.length] = 0;   //最后增加个高度为0的柱子，以便把单调栈里面的都弹出去。
+        Stack<Integer> stack = new Stack<>(); //存储序号
+        int maxS = 0;
+        for (int i = 0; i < heightn.length; i++) {
+            while (!stack.isEmpty() && heightn[i] < heightn[stack.peek()]){  //一直出栈 直到遇见小的
+                int temp = stack.pop();
+                maxS = Math.max(maxS, ((stack.isEmpty() ? i : (i - stack.peek() - 1) ) * heights[temp]));//求包含heights[tmo]的面积
+            }
+            stack.push(i);//递增push
+        }
+        return maxS;
+    }
+
 }
