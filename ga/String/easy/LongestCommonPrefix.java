c5c4824f2f6dfade68b0a8d9c1fbf290dbd10bee
kukubao207 <502914814@qq.com>
2019-09-01 11:18:21 +0800

    故事的小黄花

---
+++ ga/String/easy/LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+package String.easy;
+//14. Longest Common Prefix
+//Write a function to find the longest common prefix string amongst an array of strings.
+//
+//If there is no common prefix, return an empty string "".
+//
+//Example 1:
+//
+//Input: ["flower","flow","flight"]
+//Output: "fl"
+//Example 2:
+//
+//Input: ["dog","racecar","car"]
+//Output: ""
+//Explanation: There is no common prefix among the input strings.
+//Note:
+//
+//All given inputs are in lowercase letters a-z.
+//
+//Accepted
+//529,494
+//Submissions
+//1,554,893
+public class LongestCommonPrefix {
+}
