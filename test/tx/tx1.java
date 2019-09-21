bc0fa1251a224fa702bf653badfdface0d3c78e8
kukubao207 <502914814@qq.com>
2019-09-21 16:18:42 +0800

    commit

--- test/tx/tx1.java
+++ test/tx/tx1.java
@@ -3,41 +3,41 @@ package tx;
 import java.util.*;
 
 public class tx1 {
+    private static class Man {
+        int num;
+        int time;
+
+        public Man(int num, int time) {
+            this.num = num;
+            this.time = time;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
-        int a[] = new int[n];
-        int w[] = new int[n];
-        for (int i = 0; i < n; i++)
-            a[i] = sc.nextInt();
-        for (int i = 0; i < n; i++)
-            w[i] = sc.nextInt();
-        int sum = 0;
-        for (int weight : w) {
-            sum += weight;
-        }
-        double p[] = new double[n];
+        List<Man> manList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            p[i] = w[i] / (double)sum;
+            manList.add(new Man(sc.nextInt(), sc.nextInt()));
         }
-        int fromZeroRemain = lastRemain(n, m);
-        double res = 0;
-        for (int i = 0; i < n; i++) {
-            int idx = (fromZeroRemain + i) % n;
-//            System.out.println(i + "," + idx);
-            if (a[idx] == 1)
-                res += p[i];
+        Collections.sort(manList, new Comparator<Man>() {
+            @Override
+            public int compare(Man o1, Man o2) {
+                return o1.time - o2.time;
+            }
+        });
+        int i = 0;
+        int j = manList.size() - 1;
+        int minTime = Integer.MAX_VALUE;
+        while (i <= j && manList.get(i).num > 0 && manList.get(j).num > 0) {
+            manList.get(i).num--;
+            manList.get(j).num--;
+            minTime = Math.min(manList.get(i).time + manList.get(j).time, minTime);
+            if (manList.get(i).num == 0)
+                i++;
+            if (manList.get(j).num == 0)
+                j--;
         }
-        System.out.println(String.format("%.5f", res));
-    }
-
-    public static int lastRemain(int n, int m) {
-        if (n < 1 || m < 1)
-            return -1;
-        int last = 0;
-        for (int i = 2; i <= n; i++)
-            last = (last + m) % i;
-        return last;
+        System.out.println(minTime);
     }
 }
--- test/tx/tx3.java
+++ test/tx/tx3.java
@@ -5,45 +5,45 @@ import java.util.*;
 public class tx3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] a = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = sc.nextInt();
-            }
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int k = sc.nextInt();
+            int num[] = new int[k];
+            for (int j = 0; j < k; j++)
+                num[j] = sc.nextInt();
+            Arrays.sort(num);
+            divide(num);
         }
-        int[][] dp = new int[n][n];
-        dp[0][0] = 0;
-        for (int i = 1; i < n; i++) {
-            if (a[0][i] == 1)
-                dp[0][i] = dp[0][i - 1] + 1;
-            else
-                dp[0][i] = dp[0][i - 1];
-        }
-        for (int i = 1; i < n; i++) {
-            if (a[i][0] == 1)
-                dp[i][0] = dp[i - 1][0] + 1;
-            else
-                dp[i][0] = dp[i - 1][0];
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                if (a[i][j] == 1)
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
-                else
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-        for (int i = 1; i < n - 1; i++) {
-
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(dp[i][j] + ",");
-            }
-            System.out.println();
-        }
-        System.out.println(dp[n - 1][n - 1]);
     }
 
+    static void divide(int num[]) {
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            sum += num[i];
+        }
+        boolean f[][] = new boolean[num.length + 1][sum / 2 + 1];
+        for (int i = 0; i < num.length; i++)
+            for (int j = 0; j < sum / 2 + 1; j++)
+                f[i][j] = false;
+
+        f[0][0] = true;
+        for (int k = 0; k < num.length; k++) {
+            for (int i = Math.min(k , num.length / 2); i >= 1; i--) {
+                for (int j = 0; j <= sum / 2; j++) {
+                    if (j >= num[k] && f[i - 1][j - num[k]])
+                        f[i][j] = true;
+                }
+            }
+        }
+        for (int i = sum / 2; i >= 0; i--) {
+            if (f[num.length / 2][i]) {
+                int[] res = new int[2];
+                res[0] = i;
+                res[1] = sum - i;
+                Arrays.sort(res);
+                System.out.println(res[0] + " " + res[1]);
+                break;
+            }
+        }
+    }
 }
