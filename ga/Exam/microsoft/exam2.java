c565c103a5ffc81fea5de27eb8f98c09ea056ddc
kukubao207 <502914814@qq.com>
2019-09-23 22:06:29 +0800

    Microsoft

--- ga/Exam/microsoft/exam2.java
+++ ga/Exam/microsoft/exam2.java
@@ -13,29 +13,28 @@ public class exam2 {
     }
 
     public static void test() {
-        int D = 10;
-        int k = 1;
+        int D = 11;
+        int k = 3;
         System.out.print(GetMinDays(D, k));
     }
 
     //dp[i]定义为将i为目标的最少天数
-    public static int GetMinDays(int D, int k){
+    public static int GetMinDays(int D, int k) {
         int[] primes = getPrimeNum(k);
         int[] dp = new int[D + 1];
         Arrays.fill(dp, -1);
-        for(int i = 0; i < k; i++){
+        for (int i = 0; i < k; i++) {
             dp[primes[i]] = 1;
         }
 
-        for(int i = 2; i <= D; i++){
-            if(dp[i] == 1)
+        for (int i = 2; i <= D; i++) {
+            if (dp[i] == 1)
                 continue;
-            else{
-                int index = GetClosestMaxNum(i, primes, k);
-                for(int j = index; j >= 0; j--){
-                    if(dp[i - primes[j]] == -1)
+            else {
+                for (int j = k - 1; j >= 0; j--) {
+                    if (primes[j] > i || dp[i - primes[j]] == -1)
                         continue;
-                    else{
+                    else {
                         dp[i] = dp[i - primes[j]] + 1;
                         break;
                     }
@@ -45,12 +44,6 @@ public class exam2 {
         return dp[D];
     }
 
-    public static int GetClosestMaxNum(int i, int[] primes, int k){
-        for(int j = k - 1; j >= 0; j--)
-            if(primes[j] <= i)
-                return j;
-        return -1;
-    }
 
     public static int[] getPrimeNum(int k) {
         int count = 0;
